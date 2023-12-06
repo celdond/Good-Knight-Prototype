edede7253cee0856c0f2860dc2b107a0f09638b9
celdond <celdond@gmail.com>
2023-12-05 22:54:57 -0800

    platforms are phasable

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -4,7 +4,7 @@ using System;
 public partial class player : CharacterBody2D
 {
 	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float JumpVelocity = -450.0f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
@@ -13,13 +13,21 @@ public partial class player : CharacterBody2D
 	{
 		Vector2 velocity = Velocity;
 
+		if (Input.IsActionJustPressed("ui_down")) {
+			SetCollisionMaskValue(2, false);
+		} else if (Input.IsActionJustReleased("ui_down")) {
+			SetCollisionMaskValue(2, true);
+		}
+
 		// Add the gravity.
-		if (!IsOnFloor())
+		if (!IsOnFloor()) {
 			velocity.Y += gravity * (float)delta;
+		}
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor()) {
 			velocity.Y = JumpVelocity;
+		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
