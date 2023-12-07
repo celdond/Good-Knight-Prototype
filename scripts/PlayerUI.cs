03863f34dd9da6bfe6747fc8a7d51cce25758c81
celdond <celdond@gmail.com>
2023-12-06 19:57:42 -0800

    UI set-up

---
+++ scripts/PlayerUI.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class playerUI : Control
+{
+	public int health = 3;
+	private static int maxHealth = 3;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
