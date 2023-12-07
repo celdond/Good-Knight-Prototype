ab834cf8bf874da3191f2e6c3c549ed3f7516996
celdond <celdond@gmail.com>
2023-12-06 21:41:50 -0800

    basic enemy movement

--- scripts/Skeleton.cs
+++ scripts/Skeleton.cs
@@ -5,11 +5,21 @@ public partial class Skeleton : CharacterBody2D
 {
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 	public Vector2 direction = Vector2.Right;
+	public RayCast2D rightCheck;
+	public RayCast2D leftCheck;
+	public override void _Ready()
+	{
+		rightCheck = GetNode<RayCast2D>("LedgeCheckRight");
+		leftCheck = GetNode<RayCast2D>("LedgeCheckLeft");
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
+		bool ledgeFound = (!rightCheck.IsColliding()) || (!leftCheck.IsColliding());
 
-		if (IsOnWall()) {
+		if (IsOnWall() || ledgeFound)
+		{
 			direction *= -1;
 		}
 
